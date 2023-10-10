003d39c1086c89bbe3cc1773932e12fb7bcea63b
Cole0609 <cah062003@gmail.com>
2023-10-09 20:51:13 -0400

    finished lab

---
+++ src/labs/a3/index.js
@@ -0,0 +1,24 @@
+import Javascript from "./Javascript";
+import PathParameters from "./PathParameters";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoItems from "./Todo/TodoItem";
+import TodoList from "./Todo/TodoList";
+
+function Assignment3() {
+	return (
+		<div>
+			<h1>Assignment 3</h1>
+			<TodoList />
+			<TodoItems />
+			<ConditionalOutput />
+			<Styles />
+			<Classes />
+			<PathParameters />
+			<Javascript />
+		</div>
+	);
+}
+
+export default Assignment3;
