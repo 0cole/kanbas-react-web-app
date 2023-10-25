4d1080e4fe1fa262575f325016ea8760dcfc01a8
Cole0609 <cah062003@gmail.com>
2023-10-25 12:38:23 -0400

    finished lab

---
+++ src/labs/Practice/index.js
@@ -0,0 +1,13 @@
+function Practice() {
+	const q = { a: 123, b: 234 };
+	const w = { a: q.a, c: 456, b: 345 };
+    console.log(w);
+
+	return (
+		<div>
+			<h1>Practice</h1>
+		</div>
+	);
+}
+
+export default Practice;
--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -6,9 +6,19 @@ import ConditionalOutput from "./ConditionalOutput";
 import TodoItems from "./Todo/TodoItem";
 import TodoList from "./Todo/TodoList";
 
+import { useSelector } from "react-redux";
+
 function Assignment3() {
+	const { todos } = useSelector((state) => state.todosReducer);
 	return (
 		<div>
+			<ul className="list-group">
+				{todos.map((todo) => (
+					<li className="list-group-item" key={todo.id}>
+						{todo.title}
+					</li>
+				))}
+			</ul>
 			<h1>Assignment 3</h1>
 			<TodoList />
 			<TodoItems />
