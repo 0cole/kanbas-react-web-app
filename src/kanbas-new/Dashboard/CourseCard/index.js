f68a253fe57b5adf64a43288c17a6b64287ba5a3
Cole0609 <cah062003@gmail.com>
2023-11-02 13:55:58 -0400

    added useState for dashboard

--- src/kanbas-new/Dashboard/CourseCard/index.js
+++ src/kanbas-new/Dashboard/CourseCard/index.js
@@ -1,19 +1,56 @@
 import logo from "../../northeastern-logo.png";
 import { BiMessageSquareEdit } from "react-icons/bi";
+import { BiSave } from "react-icons/bi";
 import "./index.css";
 import { Link } from "react-router-dom";
 import { RxCross1 } from "react-icons/rx";
+import React, { useState } from "react";
 
 function CourseCard(props) {
-	const { course, deleteCourse, setCourse } = props;
+	const { course, deleteCourse, setCourse, updateCourse } = props;
+
+	const [isEditing, setIsEditing] = useState(false);
+	const [editedName, setEditedName] = useState(course.name);
+	const [editedNumber, setEditedNumber] = useState(course.number);
 
 	return (
 		<Link to={`/Kanbas/Courses/${course._id}`} className="card wd-course-card">
 			<img className="card-img-top wd-course-card-image" src={logo} alt="Northeastern University logo" />
 			<div className="card-body wd-course-card-text">
 				<div className="wd-course-card-body-top">
-					<p className="wd-course-name">{course.name}</p>
-					<p className="wd-course-text">{course.number}</p>
+					{isEditing ? (
+						<>
+							<input
+								className="wd-course-edit-contents"
+								value={editedName}
+								onClick={(event) => {
+									event.preventDefault();
+									event.stopPropagation();
+								}}
+								onChange={(event) => {
+									setEditedName(event.target.value);
+									setCourse({ ...course, name: event.target.value });
+								}}
+							/>
+							<input
+								className="wd-course-edit-contents"
+								value={editedNumber}
+								onClick={(event) => {
+									event.preventDefault();
+									event.stopPropagation();
+								}}
+								onChange={(event) => {
+									setEditedNumber(event.target.value);
+									setCourse({ ...course, number: event.target.value });
+								}}
+							/>
+						</>
+					) : (
+						<>
+							<p className="wd-course-name">{course.name}</p>
+							<p className="wd-course-number">{course.number}</p>
+						</>
+					)}
 				</div>
 				<div className="wd-course-card-body-bottom">
 					<button
@@ -29,10 +66,17 @@ function CourseCard(props) {
 						className="btn btn-outline-danger wd-course-card-button"
 						onClick={(event) => {
 							event.preventDefault();
-							setCourse(course);
+							if (isEditing) {
+								updateCourse();
+							}
+							setIsEditing(!isEditing);
 						}}
 					>
-						<BiMessageSquareEdit className="wd-course-card-icon" />
+						{isEditing ? (
+							<BiSave className="wd-course-card-icon" />
+						) : (
+							<BiMessageSquareEdit className="wd-course-card-icon" />
+						)}
 					</button>
 				</div>
 			</div>
--- src/kanbas-new/Dashboard/index.js
+++ src/kanbas-new/Dashboard/index.js
@@ -1,6 +1,7 @@
 import { React } from "react";
 import CourseCard from "./CourseCard";
 import "./index.css";
+import { BsPlusLg } from "react-icons/bs";
 
 function Dashboard({ courses, course, setCourse, addNewCourse, deleteCourse, updateCourse }) {
 	return (
@@ -12,36 +13,22 @@ function Dashboard({ courses, course, setCourse, addNewCourse, deleteCourse, upd
 				<h3>Published Courses: {courses.length} </h3>
 			</div>
 			<div className="row wd-dashboard wd-content">
-				<h1>Dashboard</h1>
-				<h5>Course</h5>
-				<input
-					value={course.name}
-					className="form-control"
-					onChange={(e) => setCourse({ ...course, name: e.target.value })}
-				/>
-				<input
-					value={course.number}
-					className="form-control"
-					onChange={(e) => setCourse({ ...course, number: e.target.value })}
-				/>
-				<input
-					value={course.startDate}
-					className="form-control"
-					type="date"
-					onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
-				/>
-				<input
-					value={course.endDate}
-					className="form-control"
-					type="date"
-					onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
-				/>
-				<button onClick={addNewCourse}>Add</button>
-				<button onClick={updateCourse}>Update</button>
-
+				<div>
+					<button className="btn btn-danger" onClick={addNewCourse}>
+						<span className="wd-btn-text">
+							{" "}
+							<BsPlusLg />
+						</span>
+					</button>
+				</div>
 				<div className="list-group d-flex flex-wrap flex-row">
 					{courses.map((course) => (
-						<CourseCard course={course} deleteCourse={deleteCourse} setCourse={setCourse}></CourseCard>
+						<CourseCard
+							course={course}
+							deleteCourse={deleteCourse}
+							setCourse={setCourse}
+							updateCourse={updateCourse}
+						></CourseCard>
 					))}
 				</div>
 			</div>
